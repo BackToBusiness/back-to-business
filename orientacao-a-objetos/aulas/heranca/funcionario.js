d12d2105ad36856be5bd9d82dd91dacdfd24042b
Marcos Paulo Salgueiro de Moraes <marcos.moraes@loft.com.br>
2020-09-01 19:26:25 -0300

    adicionando exemplos de herança

--- orientacao-a-objetos/aulas/heranca/funcionario.js
+++ orientacao-a-objetos/aulas/heranca/funcionario.js
@@ -1,5 +1,4 @@
 
-//Exemplo Alura
 class Funcionario {
     getBonificacao() {
         return 100;
@@ -13,17 +12,3 @@ class Diretor extends Funcionario {
 class DiretorTI extends Diretor {
 
 }
-
-//O problema é que quando ele instanciou um DiretorTI e 
-//chamou o método GetBonificacao ele recebou o valor de 
-//200 e não de 100 como ele esperava. Por que isso aconteceu?
-//Sim, se ele sobrescrever esse método a classe 
-//funcionará da maneira que ele espera. 
-//Porém esse código estará sendo sobrescrito em todas as 
-//camadas da hierarquia de classes. Será que é uma boa opção?
-
-/*
-Isso mesmo! Com a herança podemos herdar classes que herdam de outras classes. 
-E conforme essa cadeia cresce a complexidade do código tbm cresce. 
-Por isso é considerada uma má prática criarmos árvores de herança muito profundas.
-*/
